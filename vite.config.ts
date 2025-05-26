e539e7371cd452e5b4ed6a4d27484f11f57dc096
Akash16-web <ballu.sripathi@gmail.com>
2025-05-26 13:10:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project/', // Change this to your repo name or subfolder
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
