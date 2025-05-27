d943f39bf2b0a2239f9b93bedd312b9cc7ecb7f6
Akash16-web <ballu.sripathi@gmail.com>
2025-05-27 12:27:46 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero: React.FC = () => {
       className="relative h-screen flex items-center justify-center bg-cover bg-center"
 
        style={{ 
-        backgroundImage: "url('/images/pic.jpg')"
+        backgroundImage: "url('./dist/images/pic.jpg')"
       }}
       
     >
